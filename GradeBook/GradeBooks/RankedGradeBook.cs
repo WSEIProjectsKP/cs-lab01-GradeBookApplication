25cc7befc242842213b327e7c818a7689b99996d
KarolP1 <platek549@gmail.com>
2022-03-23 18:44:00 +0100

    ranked grade

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+
+            
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade < 80 && averageGrade >= 60)
+                return 'B';
+            else if (averageGrade < 60 && averageGrade >= 40)
+                return 'C';
+            else if (averageGrade < 40 && averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStatistics();
+        }
+
+    }
+}
